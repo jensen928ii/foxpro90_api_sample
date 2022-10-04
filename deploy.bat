8fecb21cabc3173a703b06783f22ea0459fe61ed
lamsiuho88 <88638675+lamsiuho88@users.noreply.github.com>
2022-10-04 14:47:49 -0400

    Initial commit

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+rem copy cmo.exe \\t3610\deploy
+rem copy *.fr? \\t3610\deploy
+
+copy cmo.exe \\dell7010\william
+copy *.fr? \\dell7010\william
+
+pause
---
+++ program1.prg
@@ -0,0 +1,5 @@
+SET DEFAULT TO (_vfp.activeproject.homedir)
+
+CLEAR ALL
+CLOSE ALL
+DO FORM list
